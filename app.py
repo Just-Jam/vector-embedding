9c17d67092d5d9c0e29c81204a652082a0b8d077
Just-Jam <samlyc11@gmail.com>
2024-01-13 15:13:11 +0800

    streamlit bug fixes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from dotenv import load_dotenv
-import openpyxl as xl
+import os.path
 
 from langchain.document_loaders.csv_loader import CSVLoader, DirectoryLoader, PyPDFLoader
 from langchain.vectorstores import FAISS
@@ -13,25 +13,28 @@ def vectorize_csv(filename):
     documents = loader.load()
 
     embeddings = OpenAIEmbeddings()
-    db = FAISS.from_documents(documents, embeddings)    
+    db = FAISS.from_documents(documents, embeddings)
+
+def save_uploadedfile(uploadedfile):
+    with open(os.path.join("./datasets/", uploadedfile.name), "wb") as f:
+        f.write(uploadedfile.getbuffer())
+    return st.success("Saved File:{} to tempDir".format(uploadedfile.name))
 
 # Get input file from user
-
 def main():
     st.set_page_config(
         page_title="Vectorize Your DAta", page_icon=":bird:")
 
     st.header("Vector Embeddings")
 
-    uploaded_file = st.file_uploader("Upload dataset")
+    uploaded_file = st.file_uploader("Upload dataset", type=["csv", "pdf", "docx"])
     if uploaded_file:
         st.write("filename:", uploaded_file.name)
     
     if st.button("Vectorize Data"):
         if uploaded_file.name.endswith('.csv'):
             # langchain loader requires file to be saved locally
-            wb = xl.load_workbook(uploaded_file)
-            wb.save()
+            save_uploadedfile(uploaded_file)
             vectorize_csv(uploaded_file.name)
     
 
